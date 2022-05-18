750b08a4afbb0103df1b9f93fd2e6a2766e37378
Carlos A. Cartagena-Sanchez <ccartagena@brynmawr.edu>
2022-05-18 12:39:14 -0500

    Improving the documentation.

--- python_core/tools/index_funcs.py
+++ python_core/tools/index_funcs.py
@@ -6,8 +6,9 @@ from numpy import where, abs, min
 def tindex(time_arr, time_value, delta=2e-2):
     """ Outputs the index of given timevalue
     Inputs: timearr, timevalue, delta
-        timearr: numpy array of time values
-        timevalue: time value of interest
+        time_arr: numpy 1-D array of time values
+        time_value: time value of interest
+    Optional Inputs: delta
         delta: residual error for time value
     Outputs: tind
         tind: the index of corresponding to timevalue
@@ -18,6 +19,14 @@ def tindex(time_arr, time_value, delta=2e-2):
 
 
 def tindex_min(time_arr, time_value):
+    """ Outputs the index of given timevalue based on 
+        the lowerbound.
+    Inputs: time_arr, time_value
+        time_arr: numpy 1-D array of time values
+        time_value: number of interest
+    Outputs: tind
+        tind: the index of corresponding time_value
+    """
     min_val = min(abs((time_arr)-time_value))
     tind = where(abs((time_arr)-(time_value)) == min_val)
     tind = tind[0][0]
@@ -25,19 +34,28 @@ def tindex_min(time_arr, time_value):
 
 
 def tindex_near(time_arr, time_value, threshold):
+    """ Output the index of value of interest based on 
+    the closest value to value of interest.
+    Inputs: time_arr, time_value, threshold
+        time_arr: numpy 1-D array
+        time_value: value of interest
+        threshold: threshold value
+    Outputs:
+        tinds: 1-D array of values falling within the threshold.
+    """
     tinds = where(abs(time_arr-time_value) < threshold)
     return tinds
 
 
-def first_zero(time_arr):
-    """ The first zero
-    Inputs: timearr
-        timearr: numpy array
-    Outputs: tind
-        zind: index of the first zero in timearr
+def first_zero(data):
+    """ The index which the data reaches the first zero value.
+    Inputs: data
+        data: numpy 1-D array
+    Outputs: zind
+        zind: index of the first zero in data
     """
-    for n in range(len(time_arr)):
+    for n in range(len(data)):
         zind = n
-        if time_arr[n] < 0:
+        if data[n] < 0:
             break
     return zind
